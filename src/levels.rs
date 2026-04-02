a821f9f8899885862e7f8fd134ea16b9eeae1516
JuxGD <jak@e.email>
2026-04-02 18:22:14 -0300

    i think this might work

--- src/levels.rs
+++ src/levels.rs
@@ -1,9 +1,9 @@
-pub async fn get_timely(weekly: usize) -> String {
+pub async fn get_timely(weekly: String) -> String {
     let url = "http://www.boomlings.com/database/getGJDailyLevel.php";
 
     let weekly = weekly.to_string();
 
-    let form = [("secret", "Wmfd2893gb7"), ("weekly", weekly.as_str())];
+    let form = [("secret", "Wmfd2893gb7"), ("weekly", &weekly)];
 
     let client = reqwest::Client::new();
 
@@ -21,13 +21,13 @@ pub async fn get_timely(weekly: usize) -> String {
 }
 
 pub async fn get_daily() -> String {
-    return get_timely(1).await.to_string();
+    return get_timely("-1".to_string()).await.to_string();
 }
 
 pub async fn get_weekly() -> String {
-    return get_timely(0).await.to_string();
+    return get_timely("0".to_string()).await.to_string();
 }
 
 pub async fn get_event() -> String {
-    return get_timely(2).await.to_string();
+    return get_timely("-2".to_string()).await.to_string();
 }
