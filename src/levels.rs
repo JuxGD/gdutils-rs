fe80a21e5c3c2c0c405e523844958856615ec42c
JuxGD <jak@e.email>
2026-04-02 22:25:41 -0300

    oh that's why it wasn't working

--- src/levels.rs
+++ src/levels.rs
@@ -1,12 +1,12 @@
 use crate::constants::URL;
 use crate::constants::SECRET_COMMON;
 
-pub async fn get_timely(weekly: String) -> String {
+pub async fn get_timely(ttype: String) -> String {
     let url = format!("{}/getGJDailyLevel.php", URL);
 
-    let weekly = weekly.to_string();
+    let ttype = ttype.to_string();
 
-    let form = [("secret", format!("{}", SECRET_COMMON)), ("weekly", weekly)];
+    let form = [("secret", format!("{}", SECRET_COMMON)), ("type", ttype)];
 
     let client = reqwest::Client::new();
 
@@ -24,13 +24,13 @@ pub async fn get_timely(weekly: String) -> String {
 }
 
 pub async fn get_daily() -> String {
-    return get_timely("-1".to_string()).await.to_string();
+    return get_timely(0.to_string()).await.to_string();
 }
 
 pub async fn get_weekly() -> String {
-    return get_timely("0".to_string()).await.to_string();
+    return get_timely(1.to_string()).await.to_string();
 }
 
 pub async fn get_event() -> String {
-    return get_timely("-2".to_string()).await.to_string();
+    return get_timely(2.to_string()).await.to_string();
 }
