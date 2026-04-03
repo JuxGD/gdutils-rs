976bf9785f7fe8640c15ec127c08c270afd8145c
JuxGD <jak@e.email>
2026-04-03 20:22:27 -0300

    still wip

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,3 @@
 pub const URL: &str = "http://www.boomlings.com"; // because of Rust limitations, both URL and URL_DATABASE need to be changed
 pub const URL_DATABASE: &str = "https://www.boomlings.com/database";
 pub const SECRET_COMMON: &str = "Wmfd2893gb7";
-
-use colonparse::colonparse;
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use colonparse::colonparse;
-
-fn main() {
-    colonparse("hi");
-}
