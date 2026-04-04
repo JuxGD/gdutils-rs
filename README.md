51568db52e54ada58d7bfca7f72fce0a65d53485
JuxGD <jak@e.email>
2026-04-04 10:25:45 -0300

    fix repository in cargo toml, implement user info, update readme

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
 # `gdutils`
 
-`gdutils` is a Rust crate used to fetch all sorts of data from the Geometry Dash servers.
+`gdutils` is a Rust crate intended used to fetch all sorts of data from the Geometry Dash servers.
 
-Originally made for the FluxDash bot for Fluxer
+This project is still a WIP! Contrubutions are appreciated. Check the To-do section!
+
+Originally made for the FluxDash bot for Fluxer.
+
+## To-do list / What to help with
+
+Current (or future) crate features:
+
+- [ ] Getting arbitrary level data (`gd!level`)
+  - [X] Name
+  - [X] ID
+  - [ ] Author
+  - [X] Song ID
+  - [ ] Song artist - song name (Newgrounds/Music Library)
+  - [X] Difficulty rating 
+  - [X] Stars
+  - [ ] Moons
+  - [X] Likes
+  - [X] Downloads
+- [X] Getting daily, weekly level indes & time left (`gd!daily`, `gd!weekly`) 
+- [X] Getting event level index (`gd!index`)
+- [ ] Getting arbitrary user data (`gd!user`)
+  - [ ] Username
+  - [ ] Account ID
+  - [ ] Player ID
+  - [ ] Leaderboard placement
+  - [ ] Stars & Moons
+    - [ ] Amount of each difficulty beaten
+  - [ ] Diamonds
+  - [ ] Demons
+    - [ ] Amount of each demon difficulty beaten
+  - [ ] Creator Points
+  - [ ] Icons (as data not the actual icons ofc)
+- [ ] Generating icons from icon data
 
 ## AI, Agents, LLMs
 
--- src/users.rs
+++ src/users.rs
@@ -1,12 +1,48 @@
-use crate::constants::*;
+use colonparse::hashmap_from;
+
+use crate::{constants::*, levels::Level};
+use std::fmt::{Debug};
+
+#[derive(Debug)]
+struct User {
+    username: String
+,   stars: i32
+,   moons: i32
+,   diamonds: i32
+,   demons: i32
+,   top: i32
+,   ctpoints: i32
+}
 
 // Gets user data from playerID
-async fn get_user_data(id: &str) -> String {
+pub async fn get_user(id: &str) -> User {
     let url = format!("{}/getGJUserInfo20.php", URL_DATABASE);
 
     let form = vec![("secret", SECRET_COMMON), ("targetAccountID", id)];
 
     let client = reqwest::Client::new();
 
-    return String::from(client.post(url).form(&form).send().await.unwrap().text().await.unwrap());
+    let request = String::from(client.post(url)
+        .form(&form)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
+    );
+
+    let user_hashmap = hashmap_from(request);
+
+    let user = User {
+        username: String::from(user_hashmap.get("1").unwrap())
+    ,   stars: user_hashmap["1"].parse::<i32>().unwrap()
+    ,   moons: user_hashmap["52"].parse::<i32>().unwrap()
+    ,   diamonds: user_hashmap["46"].parse::<i32>().unwrap()
+    ,   demons: user_hashmap["4"].parse::<i32>().unwrap()
+    ,   top: user_hashmap["6"].parse::<i32>().unwrap()
+    ,   ctpoints: user_hashmap["8"].parse::<i32>().unwrap()
+    };
+
+    return user;
 }
